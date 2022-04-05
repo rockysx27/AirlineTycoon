9f04ee05872a45fea22cec00ee87493f89010cc2
Merten Popp <webmaster@merten-popp.de>
2022-04-05 19:06:34 +0200

    Use fixed width data types

--- src/GameFrame.h
+++ src/GameFrame.h
@@ -23,7 +23,6 @@ class GameFrame {
     void UpdateWindow() const;
     void UpdateFrameSize() const;
     void RePostMessage(const CPoint &Pos) const;
-    void RePostClick(SLONG PlayerNum, UINT message, WPARAM wParam, LPARAM lParam);
     static void PrepareFade(void);
     static bool Pause(bool fPause); // AG
     void TranslatePointToGameSpace(CPoint *p) const;
@@ -33,7 +32,6 @@ class GameFrame {
     // ClassWizard generated virtual function overrides
     //{{AFX_VIRTUAL(GameFrame)
   protected:
-    virtual BOOL OnCommand(WPARAM wParam, LPARAM lParam);
     //}}AFX_VIRTUAL
 
     // Implementation
