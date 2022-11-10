aa848436b7b9f19d8cab1782a3abbf9b2bcf880f
WizzardMaker <wizzardmaker@gmail.com>
2022-11-10 23:35:07 +0100

    made decoded RL file saving optional with /savedata flag

--- src/TeakLibW/TeakFile.cpp
+++ src/TeakLibW/TeakFile.cpp
@@ -140,6 +140,8 @@ CRLEReader::CRLEReader(const char *path) : Ctx(nullptr), SeqLength(0), SeqUsed(0
 
 CRLEReader::~CRLEReader() { Close(); }
 
+bool CRLEReader::AlwaysSaveAsPlainText = false;
+
 bool CRLEReader::Close() {
     if (Ctx == nullptr) {
         return false;
@@ -250,9 +252,9 @@ BUFFER_V<BYTE> LoadCompleteFile(char const *path) {
         return buffer;
     }
 
-    if (reader.getIsRLE()) {
-        CRLEReader konverter(path);
-        konverter.SaveAsPlainText();
+    if (reader.getIsRLE() && CRLEReader::AlwaysSaveAsPlainText) {
+        CRLEReader converter(path);
+        converter.SaveAsPlainText();
     }
 
     return buffer;
