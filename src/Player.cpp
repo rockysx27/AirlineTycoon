rockysx27/AirlineTycoon

commit a50c1b94a0a400480533531c2fb8656938c513d0

Bot: Sabotage using GameMechanic

src/Player.cpp
@@ -4598,193 +4598,89 @@ void PLAYER::RobotExecuteAction() {
                 dislike != -1 && dislike != PlayerNum &&
                 (ArabHints < 80 || (ArabHints < 90 && Credit < 1000000 && Money > 3000000 && RobotUse(ROBOT_USE_SABO_AFFORD_FINE) && (Sim.Date & 3) == 0)) &&
                 (Sim.Players.Players[dislike].Owner == 1 || RobotUse(ROBOT_USE_MUCH_SABOTAGE))) {
+
                 SLONG SecurityAnnoiance = 0;
+                SLONG targetMode = 0;
+                GameMechanic::CheckSabotage ret = {};
 
                 for (SLONG pass = 1; pass <= 2 + 3 * static_cast<SLONG>(RobotUse(ROBOT_USE_EXTREME_SABOTAGE)); pass++) {
                     switch (Sim.GetHour() % 3) {
                     case 0:
                         if (Sim.Players.Players[dislike].Planes.GetNumUsed() > 0) {
-                            ArabOpfer = dislike;
-                            ArabMode = min(4, ArabTrust);
-                            if (pass > 2 && ArabMode > 0) {
-                                ArabMode = LocalRandom.Rand(ArabMode);
+                            targetMode = min(4, ArabTrust);
+                            if (pass > 2 && targetMode > 0) {
+                                targetMode = LocalRandom.Rand(targetMode);
                             }
 
                             if (RobotUse(ROBOT_USE_MILDER_SABOTAGE) && ArabTrust >= 2 && LocalRandom.Rand(3) != 0) {
-                                ArabMode = min(ArabMode, 2);
+                                targetMode = min(targetMode, 2);
                             }
                             if (RobotUse(ROBOT_USE_MILD_SABOTAGE) && ArabTrust >= 3 && LocalRandom.Rand(3) != 0) {
-                                ArabMode = min(ArabMode, 3);
+                                targetMode = min(targetMode, 3);
                             }
 
-                            ArabActive = FALSE;
-                            ArabPlane = Sim.Players.Players[dislike].Planes.GetRandomUsedIndex(&LocalRandom);
+                            ArabPlaneSelection = Sim.Players.Players[dislike].Planes.GetRandomUsedIndex(&LocalRandom);
+                            ret = GameMechanic::checkPrerequisitesForSaboteurJob(*this, 0, targetMode);
+                            if (ret.result == GameMechanic::CheckSabotageResult::Ok) {
+                                GameMechanic::activateSaboteurJob(*this);
+                            } else if (ret.result == GameMechanic::CheckSabotageResult::DeniedSecurity) {
+                                SecurityAnnoiance++;
+                            }
                         }
                         break;
 
                     case 1:
-                        ArabOpfer2 = dislike;
-                        ArabMode2 = min(4, ArabTrust);
-                        if (pass > 2 && ArabMode2 > 0) {
-                            ArabMode2 = LocalRandom.Rand(ArabMode2);
+                        targetMode = min(4, ArabTrust);
+                        if (pass > 2 && targetMode > 0) {
+                            targetMode = LocalRandom.Rand(targetMode);
                         }
 
                         if (RobotUse(ROBOT_USE_MILDER_SABOTAGE) && LocalRandom.Rand(3) != 0) {
-                            ArabMode2 = 1;
+                            targetMode = 1;
                         }
                         if (RobotUse(ROBOT_USE_MILD_SABOTAGE) && ArabTrust >= 3 && LocalRandom.Rand(3) != 0) {
-                            ArabMode2 = min(ArabMode2, 3);
+                            targetMode = min(targetMode, 3);
                         }
-                        if (ArabMode2 == 1 && ArabTrust > ArabMode2 && rand() % 2 == 0) {
-                            ArabMode2++;
+                        if (targetMode == 1 && ArabTrust > targetMode && rand() % 2 == 0) {
+                            targetMode++;
                         }
-                        if (ArabMode2 == 2 && (Sim.Players.Players[ArabOpfer2].HasItem(ITEM_LAPTOP) == 0)) {
-                            ArabMode2--;
+                        if (targetMode == 2 && (Sim.Players.Players[ArabOpfer2].HasItem(ITEM_LAPTOP) == 0)) {
+                            targetMode--;
+                        }
+
+                        ret = GameMechanic::checkPrerequisitesForSaboteurJob(*this, 1, targetMode);
+                        if (ret.result == GameMechanic::CheckSabotageResult::Ok) {
+                            GameMechanic::activateSaboteurJob(*this);
+                        } else if (ret.result == GameMechanic::CheckSabotageResult::DeniedSecurity) {
+                            SecurityAnnoiance++;
                         }
                         break;
 
                     case 2:
                         if (Sim.Players.Players[dislike].Planes.GetNumUsed() > 0) {
-                            ArabOpfer3 = dislike;
-                            ArabMode3 = min(5, ArabTrust);
-                            if (pass > 2 && ArabMode3 > 0) {
-                                ArabMode3 = LocalRandom.Rand(ArabMode3);
+                            targetMode = min(5, ArabTrust);
+                            if (pass > 2 && targetMode > 0) {
+                                targetMode = LocalRandom.Rand(targetMode);
                             }
 
                             if (RobotUse(ROBOT_USE_MILDER_SABOTAGE) && LocalRandom.Rand(3) != 0) {
-                                ArabMode3 = 1;
+                                targetMode = 1;
                             }
-                            if (ArabMode3 == 5) {
-                                ArabPlane = Sim.Players.Players[dislike].Planes.GetRandomUsedIndex(&LocalRandom);
+
+                            if (targetMode == 5) {
+                                ArabPlaneSelection = Sim.Players.Players[dislike].Planes.GetRandomUsedIndex(&LocalRandom);
+                            }
+                            ret = GameMechanic::checkPrerequisitesForSaboteurJob(*this, 2, targetMode);
+                            if (ret.result == GameMechanic::CheckSabotageResult::Ok) {
+                                GameMechanic::activateSaboteurJob(*this);
+                            } else if (ret.result == GameMechanic::CheckSabotageResult::DeniedSecurity) {
+                                SecurityAnnoiance++;
                             }
                         }
                         break;
                     default:
                         hprintf("Player.cpp: Default case should not be reached.");
                         DebugBreak();
-                    }
-
-                    if (ArabMode != 0 && Money - SabotagePrice[ArabMode] < DEBT_LIMIT) {
-                        ArabMode = 0;
-                    }
-                    if (ArabMode2 != 0 && Money - SabotagePrice2[ArabMode2] < DEBT_LIMIT) {
-                        ArabMode2 = 0;
-                    }
-                    if (ArabMode3 != 0 && Money - SabotagePrice3[ArabMode3] < DEBT_LIMIT) {
-                        ArabMode3 = 0;
-                    }
-
-                    // Wegen Security-Office:
-                    if (ArabMode == 1 && ((Sim.Players.Players[ArabOpfer].SecurityFlags & (1 << 6)) != 0U)) {
-                        ArabMode = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode == 2 && ((Sim.Players.Players[ArabOpfer].SecurityFlags & (1 << 6)) != 0U)) {
-                        ArabMode = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode == 3 && ((Sim.Players.Players[ArabOpfer].SecurityFlags & (1 << 7)) != 0U)) {
-                        ArabMode = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode == 4 && ((Sim.Players.Players[ArabOpfer].SecurityFlags & (1 << 7)) != 0U)) {
-                        ArabMode = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode == 5 && ((Sim.Players.Players[ArabOpfer].SecurityFlags & (1 << 6)) != 0U)) {
-                        ArabMode = 0;
-                        SecurityAnnoiance++;
-                    }
-
-                    if (ArabMode2 == 1 && ((Sim.Players.Players[ArabOpfer2].SecurityFlags & (1 << 0)) != 0U)) {
-                        ArabMode2 = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode2 == 2 && ((Sim.Players.Players[ArabOpfer2].SecurityFlags & (1 << 1)) != 0U)) {
-                        ArabMode2 = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode2 == 3 && ((Sim.Players.Players[ArabOpfer2].SecurityFlags & (1 << 0)) != 0U)) {
-                        ArabMode2 = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode2 == 4 && ((Sim.Players.Players[ArabOpfer2].SecurityFlags & (1 << 2)) != 0U)) {
-                        ArabMode2 = 0;
-                        SecurityAnnoiance++;
-                    }
-
-                    if (ArabMode3 == 1 && ((Sim.Players.Players[ArabOpfer3].SecurityFlags & (1 << 8)) != 0U)) {
-                        ArabMode3 = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode3 == 2 && ((Sim.Players.Players[ArabOpfer3].SecurityFlags & (1 << 5)) != 0U)) {
-                        ArabMode3 = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode3 == 3 && ((Sim.Players.Players[ArabOpfer3].SecurityFlags & (1 << 5)) != 0U)) {
-                        ArabMode3 = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode3 == 4 && ((Sim.Players.Players[ArabOpfer3].SecurityFlags & (1 << 3)) != 0U)) {
-                        ArabMode3 = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode3 == 5 && ((Sim.Players.Players[ArabOpfer3].SecurityFlags & (1 << 8)) != 0U)) {
-                        ArabMode3 = 0;
-                        SecurityAnnoiance++;
-                    }
-                    if (ArabMode3 == 6 && ((Sim.Players.Players[ArabOpfer3].SecurityFlags & (1 << 4)) != 0U)) {
-                        ArabMode3 = 0;
-                        SecurityAnnoiance++;
-                    }
-
-                    // Wegen Security-Office II:
-                    if (ArabMode == 1) {
-                        Sim.Players.Players[ArabOpfer].SecurityNeeded |= (1 << 6);
-                    }
-                    if (ArabMode == 2) {
-                        Sim.Players.Players[ArabOpfer].SecurityNeeded |= (1 << 6);
-                    }
-                    if (ArabMode == 3) {
-                        Sim.Players.Players[ArabOpfer].SecurityNeeded |= (1 << 7);
-                    }
-                    if (ArabMode == 4) {
-                        Sim.Players.Players[ArabOpfer].SecurityNeeded |= (1 << 7);
-                    }
-                    if (ArabMode == 5) {
-                        Sim.Players.Players[ArabOpfer].SecurityNeeded |= (1 << 6);
-                    }
-
-                    if (ArabMode2 == 1) {
-                        Sim.Players.Players[ArabOpfer2].SecurityNeeded |= (1 << 0);
-                    }
-                    if (ArabMode2 == 2) {
-                        Sim.Players.Players[ArabOpfer2].SecurityNeeded |= (1 << 1);
-                    }
-                    if (ArabMode2 == 3) {
-                        Sim.Players.Players[ArabOpfer2].SecurityNeeded |= (1 << 0);
-                    }
-                    if (ArabMode2 == 4) {
-                        Sim.Players.Players[ArabOpfer2].SecurityNeeded |= (1 << 2);
-                    }
-
-                    if (ArabMode3 == 1) {
-                        Sim.Players.Players[ArabOpfer3].SecurityNeeded |= (1 << 8);
-                    }
-                    if (ArabMode3 == 2) {
-                        Sim.Players.Players[ArabOpfer3].SecurityNeeded |= (1 << 5);
-                    }
-                    if (ArabMode3 == 3) {
-                        Sim.Players.Players[ArabOpfer3].SecurityNeeded |= (1 << 5);
-                    }
-                    if (ArabMode3 == 4) {
-                        Sim.Players.Players[ArabOpfer3].SecurityNeeded |= (1 << 3);
-                    }
-                    if (ArabMode3 == 5) {
-                        Sim.Players.Players[ArabOpfer3].SecurityNeeded |= (1 << 8);
-                    }
-                    if (ArabMode3 == 6) {
-                        Sim.Players.Players[ArabOpfer3].SecurityNeeded |= (1 << 4);
                     }
 
                     if ((ArabMode != 0) || (ArabMode2 != 0) || (ArabMode3 != 0)) {
@@ -4801,25 +4697,6 @@ void PLAYER::RobotExecuteAction() {
                         SIM::SendSimpleMessage(ATNET_TAKETHING, 0, ITEM_ZANGE);
                         RobotActions[1].ActionId = ACTION_VISITSECURITY2;
                     }
-                }
-
-                // Kosten verbuchen
-                SLONG mode = 0;
-                SLONG preis = 0;
-                if (ArabMode != 0) {
-                    mode = ArabMode;
-                    preis = SabotagePrice[mode - 1];
-                } else if (ArabMode2 != 0) {
-                    mode = ArabMode2;
-                    preis = SabotagePrice2[mode - 1];
-                } else if (ArabMode3 != 0) {
-                    mode = ArabMode3;
-                    preis = SabotagePrice3[mode - 1];
-                }
-                if (mode != 0) {
-                    ChangeMoney(-preis, 2080, "");
-                    DoBodyguardRabatt(preis);
-                    ArabTrust = min(6, mode + 1);
                 }
 
                 WorkCountdown = 20 * 10;

src/Dialog.cpp
@@ -351,11 +351,11 @@ BOOL CStdRaum::PreLButtonDown(CPoint point) {
             case 1054:
             case 1055: {
                 SLONG number = (id - 1050);
-                auto res = GameMechanic::checkPrerequisitesForSaboteurJob(qPlayer, 0, number);
-                if (res.dialogParam.empty()) {
-                    MakeSayWindow(0, TOKEN_SABOTAGE, res.dialogID, pFontPartner);
+                auto ret = GameMechanic::checkPrerequisitesForSaboteurJob(qPlayer, 0, number);
+                if (ret.dialogParam.empty()) {
+                    MakeSayWindow(0, TOKEN_SABOTAGE, ret.dialogID, pFontPartner);
                 } else {
-                    MakeSayWindow(0, TOKEN_SABOTAGE, res.dialogID, pFontPartner, (LPCTSTR)res.dialogParam);
+                    MakeSayWindow(0, TOKEN_SABOTAGE, ret.dialogID, pFontPartner, (LPCTSTR)ret.dialogParam);
                 }
                 break;
             }
@@ -376,13 +376,13 @@ BOOL CStdRaum::PreLButtonDown(CPoint point) {
             case 1253:
             case 1254: {
                 SLONG number = (id - 1250);
-                auto res = GameMechanic::checkPrerequisitesForSaboteurJob(qPlayer, 1, number);
-                if (res.dialogParam.empty()) {
-                    MakeSayWindow(0, TOKEN_SABOTAGE, res.dialogID, pFontPartner);
+                auto ret = GameMechanic::checkPrerequisitesForSaboteurJob(qPlayer, 1, number);
+                if (ret.dialogParam.empty()) {
+                    MakeSayWindow(0, TOKEN_SABOTAGE, ret.dialogID, pFontPartner);
                 } else {
-                    MakeSayWindow(0, TOKEN_SABOTAGE, res.dialogID, pFontPartner, (LPCTSTR)res.dialogParam);
+                    MakeSayWindow(0, TOKEN_SABOTAGE, ret.dialogID, pFontPartner, (LPCTSTR)ret.dialogParam);
                 }
-                if (res.OK) {
+                if (ret.result == GameMechanic::CheckSabotageResult::Ok) {
                     GameMechanic::activateSaboteurJob(qPlayer);
                 }
                 break;
@@ -395,13 +395,13 @@ BOOL CStdRaum::PreLButtonDown(CPoint point) {
             case 1085:
             case 1086: {
                 SLONG number = (id - 1080);
-                auto res = GameMechanic::checkPrerequisitesForSaboteurJob(qPlayer, 2, number);
-                if (res.dialogParam.empty()) {
-                    MakeSayWindow(0, TOKEN_SABOTAGE, res.dialogID, pFontPartner);
+                auto ret = GameMechanic::checkPrerequisitesForSaboteurJob(qPlayer, 2, number);
+                if (ret.dialogParam.empty()) {
+                    MakeSayWindow(0, TOKEN_SABOTAGE, ret.dialogID, pFontPartner);
                 } else {
-                    MakeSayWindow(0, TOKEN_SABOTAGE, res.dialogID, pFontPartner, (LPCTSTR)res.dialogParam);
+                    MakeSayWindow(0, TOKEN_SABOTAGE, ret.dialogID, pFontPartner, (LPCTSTR)ret.dialogParam);
                 }
-                if (res.OK) {
+                if (ret.result == GameMechanic::CheckSabotageResult::Ok) {
                     if (id == 1085) {
                         MenuDialogReEntryB = 1090;
                         MenuStart(MENU_SABOTAGEPLANE);

src/GameMechanic.cpp
@@ -146,85 +146,93 @@ SLONG GameMechanic::setSaboteurTarget(PLAYER &qPlayer, SLONG target) {
 
 GameMechanic::CheckSabotage GameMechanic::checkPrerequisitesForSaboteurJob(PLAYER &qPlayer, SLONG type, SLONG number) {
     if (checkSaboteurBusy(qPlayer)) {
-        return {false, 0};
+        return {CheckSabotageResult::DeniedInvalidParam, 0};
     }
 
     auto victimID = qPlayer.ArabOpferSelection;
     if (type == 0) {
         if (number < 1 || number >= 6) {
             hprintf("GameMechanic::checkPrerequisitesForSaboteurJob: Invalid job number.");
-            return {false, 0};
+            return {CheckSabotageResult::DeniedInvalidParam, 0};
         }
+
+        /* check security */
         if (number == 1 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 6)) != 0U)) {
-            return {false, 2096, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2096, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 2 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 6)) != 0U)) {
-            return {false, 2096, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2096, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 3 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 7)) != 0U)) {
-            return {false, 2097, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2097, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 4 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 7)) != 0U)) {
-            return {false, 2097, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2097, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 5 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 6)) != 0U)) {
-            return {false, 2096, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2096, Sim.Players.Players[victimID].AirlineX};
         } else if (qPlayer.Money - SabotagePrice[number - 1] < DEBT_LIMIT) {
-            return {false, 6000};
-        } else {
-            qPlayer.ArabModeSelection = {type, number};
-            return {true, 1060};
+            return {CheckSabotageResult::DeniedNotEnoughMoney, 6000};
         }
-    } else if (type == 1) {
+
+        qPlayer.ArabModeSelection = {type, number};
+        return {CheckSabotageResult::Ok, 1060};
+    }
+    if (type == 1) {
         if (number < 1 || number >= 5) {
             hprintf("GameMechanic::checkPrerequisitesForSaboteurJob: Invalid job number.");
-            return {false, 0};
+            return {CheckSabotageResult::DeniedInvalidParam, 0};
         }
+
+        /* check security */
         if (number == 1 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 0)) != 0U)) {
-            return {false, 2090, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2090, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 2 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 1)) != 0U)) {
-            return {false, 2091, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2091, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 3 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 0)) != 0U)) {
-            return {false, 2090, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2090, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 4 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 2)) != 0U)) {
-            return {false, 2092, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2092, Sim.Players.Players[victimID].AirlineX};
         } else if (qPlayer.Money - SabotagePrice2[number - 1] < DEBT_LIMIT) {
-            return {false, 6000};
+            return {CheckSabotageResult::DeniedNotEnoughMoney, 6000};
         } else if (number == 2 && (Sim.Players.Players[victimID].HasItem(ITEM_LAPTOP) == 0)) {
-            return {false, 1300};
-        } else {
-            qPlayer.ArabModeSelection = {type, number};
-            return {true, 1160};
+            return {CheckSabotageResult::DeniedNoLaptop, 1300};
         }
-    } else if (type == 2) {
+
+        qPlayer.ArabModeSelection = {type, number};
+        return {CheckSabotageResult::Ok, 1160};
+    }
+    if (type == 2) {
         if (number < 1 || number >= 7) {
             hprintf("GameMechanic::checkPrerequisitesForSaboteurJob: Invalid job number.");
-            return {false, 0};
+            return {CheckSabotageResult::DeniedInvalidParam, 0};
         }
+
+        /* check security */
         if (number == 1 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 8)) != 0U)) {
-            return {false, 2098, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2098, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 2 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 5)) != 0U)) {
-            return {false, 2095, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2095, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 3 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 5)) != 0U)) {
-            return {false, 2095, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2095, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 4 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 3)) != 0U)) {
-            return {false, 2093, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2093, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 5 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 8)) != 0U)) {
-            return {false, 2098, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2098, Sim.Players.Players[victimID].AirlineX};
         } else if (number == 6 && ((Sim.Players.Players[victimID].SecurityFlags & (1 << 4)) != 0U)) {
-            return {false, 2094, Sim.Players.Players[victimID].AirlineX};
+            return {CheckSabotageResult::DeniedSecurity, 2094, Sim.Players.Players[victimID].AirlineX};
         } else if (qPlayer.Money - SabotagePrice3[number - 1] < DEBT_LIMIT) {
-            return {false, 6000};
-        } else {
-            qPlayer.ArabModeSelection = {type, number};
-            return {true, 1160};
+            return {CheckSabotageResult::DeniedNotEnoughMoney, 6000};
         }
+
+        qPlayer.ArabModeSelection = {type, number};
+        return {CheckSabotageResult::Ok, 1160};
     }
     hprintf("GameMechanic::checkPrerequisitesForSaboteurJob: Invalid type.");
-    return {false, 0};
+    return {CheckSabotageResult::DeniedInvalidParam, 0};
 }
 
 bool GameMechanic::activateSaboteurJob(PLAYER &qPlayer) {
     SLONG type = qPlayer.ArabModeSelection.first;
     SLONG number = qPlayer.ArabModeSelection.second;
-    auto res = checkPrerequisitesForSaboteurJob(qPlayer, type, number);
-    if (!res.OK) {
+    auto ret = checkPrerequisitesForSaboteurJob(qPlayer, type, number);
+    if (ret.result != CheckSabotageResult::Ok) {
         return false;
     }
 
@@ -232,6 +240,10 @@ bool GameMechanic::activateSaboteurJob(PLAYER &qPlayer) {
     if (type == 0) {
         if (number < 1 || number >= 6) {
             hprintf("GameMechanic::activateSaboteurJob: Invalid job number.");
+            return false;
+        }
+        if (qPlayer.ArabPlaneSelection == -1) {
+            hprintf("GameMechanic::activateSaboteurJob: Need plane or route ID for this job");
             return false;
         }
 
@@ -255,6 +267,7 @@ bool GameMechanic::activateSaboteurJob(PLAYER &qPlayer) {
         qPlayer.ArabMode = number;
         qPlayer.ArabModeSelection = {};
 
+        qPlayer.ArabActive = FALSE;
         qPlayer.ArabPlane = qPlayer.ArabPlaneSelection;
         qPlayer.ArabPlaneSelection = -1;
 
@@ -265,8 +278,6 @@ bool GameMechanic::activateSaboteurJob(PLAYER &qPlayer) {
         SIM::SendSimpleMessage(ATNET_CHANGEMONEY, 0, Sim.localPlayer, -SabotagePrice[qPlayer.ArabMode - 1], -1);
 
         qPlayer.NetSynchronizeSabotage();
-
-        return true;
     } else if (type == 1) {
         if (number < 1 || number >= 5) {
             hprintf("GameMechanic::activateSaboteurJob: Invalid job number.");
@@ -291,6 +302,7 @@ bool GameMechanic::activateSaboteurJob(PLAYER &qPlayer) {
         qPlayer.ArabMode2 = number;
         qPlayer.ArabModeSelection = {};
 
+        qPlayer.ArabActive = FALSE;
         qPlayer.ArabPlane = qPlayer.ArabPlaneSelection;
         qPlayer.ArabPlaneSelection = -1;
 
@@ -301,11 +313,13 @@ bool GameMechanic::activateSaboteurJob(PLAYER &qPlayer) {
         SIM::SendSimpleMessage(ATNET_CHANGEMONEY, 0, Sim.localPlayer, -SabotagePrice2[qPlayer.ArabMode2 - 1], -1);
 
         qPlayer.NetSynchronizeSabotage();
-
-        return true;
     } else if (type == 2) {
         if (number < 1 || number >= 5) {
             hprintf("GameMechanic::activateSaboteurJob: Invalid job number.");
+            return false;
+        }
+        if (number >= 5 && qPlayer.ArabPlaneSelection == -1) {
+            hprintf("GameMechanic::activateSaboteurJob: Need plane or route ID for this job");
             return false;
         }
 
@@ -327,6 +341,7 @@ bool GameMechanic::activateSaboteurJob(PLAYER &qPlayer) {
         qPlayer.ArabMode3 = number;
         qPlayer.ArabModeSelection = {};
 
+        qPlayer.ArabActive = FALSE;
         qPlayer.ArabPlane = qPlayer.ArabPlaneSelection;
         qPlayer.ArabPlaneSelection = -1;
 
@@ -337,11 +352,11 @@ bool GameMechanic::activateSaboteurJob(PLAYER &qPlayer) {
         SIM::SendSimpleMessage(ATNET_CHANGEMONEY, 0, Sim.localPlayer, -SabotagePrice3[qPlayer.ArabMode3 - 1], -1);
 
         qPlayer.NetSynchronizeSabotage();
-
-        return true;
+    } else {
+        hprintf("GameMechanic::activateSaboteurJob: Invalid type.");
+        return false;
     }
-    hprintf("GameMechanic::activateSaboteurJob: Invalid type.");
-    return false;
+    return true;
 }
 
 void GameMechanic::paySaboteurFine(SLONG player, SLONG opfer) {

src/GameMechanic.h
@@ -27,9 +27,10 @@ class GameMechanic {
     /* Sabotage */
     static SLONG setSaboteurTarget(PLAYER &qPlayer, SLONG target);
     static bool checkSaboteurBusy(PLAYER &qPlayer) { return (qPlayer.ArabMode != 0) || (qPlayer.ArabMode2 != 0) || (qPlayer.ArabMode3 != 0); }
+    enum class CheckSabotageResult { Ok, DeniedInvalidParam, DeniedSecurity, DeniedNotEnoughMoney, DeniedNoLaptop };
     struct CheckSabotage {
-        bool OK;
-        int dialogID;
+        CheckSabotageResult result{CheckSabotageResult::DeniedInvalidParam};
+        int dialogID{};
         CString dialogParam{};
     };
     static CheckSabotage checkPrerequisitesForSaboteurJob(PLAYER &qPlayer, SLONG type, SLONG number);

commit 9c787886f50f64f6e91c3d66075517f4f3cbeed3

Fix bug introduced in a50c1b94
Computer players would only sabotage Sunshine Airways

Also fix route configuration for evolution missions.

Saldo_freegame: Mean = 446.35M, stddev = 80.05M

src/Player.cpp
@@ -4538,6 +4538,8 @@ void PLAYER::RobotExecuteAction() {
                 SLONG targetMode = 0;
                 GameMechanic::CheckSabotage ret = {};
 
+                GameMechanic::setSaboteurTarget(*this, dislike);
+
                 for (SLONG pass = 1; pass <= 2 + 3 * static_cast<SLONG>(RobotUse(ROBOT_USE_EXTREME_SABOTAGE)); pass++) {
                     switch (Sim.GetHour() % 3) {
                     case 0:
@@ -7416,10 +7418,10 @@ bool PLAYER::RobotUse(SLONG FeatureId) const {
         break;
     case ROBOT_USE_ROUTES:
         /* SuperBot: Respects this flag */
-        pFeatureDesc = "---XXX"
+        pFeatureDesc = "---X-X"
                        "X"
-                       "X--XXX-X-X"
-                       "----------";
+                       "----XX-X-X"
+                       "XXXXXXXXXX";
         break;
     case ROBOT_USE_SUGGESTROUTES:
         /* SuperBot: Ignores this flag (what is the difference to ROBOT_USE_ROUTEMISSION?) */